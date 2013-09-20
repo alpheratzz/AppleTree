d6bb00ca817c594aa4afc3f5ba76cac9b442d22e
alpheratzz <alpheratz93@gmail.com>
2013-09-21 01:14:14 +0300

    Add seeds in apples

--- AppleTreeTest/AppleTreeTest/Apple.cs
+++ AppleTreeTest/AppleTreeTest/Apple.cs
@@ -7,5 +7,12 @@ namespace AppleTreeTest
 {
     class Apple
     {
+        private uint seed_count;
+
+        public Apple()
+        {
+            Random r = new Random();
+            seed_count = (uint)r.Next(10);
+        }
     }
 }
